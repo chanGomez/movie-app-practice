e44d2995826eae2d84738e92cd8f8e5285520cf8
Chantal <chantalgomez@pursuit.org>
2024-03-06 14:25:07 -0500

    backend completed with crub opperations

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const movies = express.Router()
-const { getAllMovies } = require("../queries/movies")
+const { getAllMovies, getMovieById, createMovie, updateMovieById, deleteMovie} = require("../queries/movies")
 
 movies.get("/", async (req, res) =>{
     try{
@@ -10,5 +10,52 @@ movies.get("/", async (req, res) =>{
         res.status(404).json({payload: error })
     }
 })
+
+movies.get("/:movieId", async  (req, res) => {
+    const {movieId} = req.params
+
+    try{
+        const foundMovie = await getMovieById(movieId)
+        res.status(200).json({payload: foundMovie})
+    }catch(e){
+        res.status(404).json({payload: e})
+    }
+})
+
+movies.post("/", async (req, res) => {
+    const body = req.body;
+
+    try {
+        const newMovie = await createMovie(body)
+        res.status(201).json({payload: newMovie})
+    } catch(e) {
+        res.status(404).json({payload: e})
+    }
+})
+
+movies.put("/:id", async (req, res) => {
+    const { id } = req.params
+    const body = req.body
+    try {
+    const updatedMoive = await updateMovieById(id, body)
+        res.status(201).json({payload: updatedMoive})
+    } catch (e) {
+        res.status(404).json({payload: e})
+    }
+})
+
+movies.delete("/:id", async (req, res) => {
+    const {id} = req.params
+
+    try {
+        const movie = await deleteMovie(id)
+        res.status(200).json({payload: movie})
+
+    } catch (error) {
+        res.status(404).json({payload: e})
+    }
+})
+
+
 
 module.exports = movies;
--- queries/movies.js
+++ queries/movies.js
@@ -8,6 +8,51 @@ const getAllMovies = async () => {
         return e
     }
 }
+
+const getMovieById = async ( movieId ) => {
+    try{
+        const movie = await db.one(`SELECT * FROM movies WHERE id = $1`, movieId )
+        return movie
+    }catch(e){
+        return e
+    }
+}
+
+const createMovie = async ({name, description, year, favorite}) => {
+    try{
+        const newMovie = await db.one("INSERT INTO movies (name, description, year, favorite) VALUES ($1, $2, $3, $4) RETURNING *", [name, description, year, favorite])
+        return newMovie
+    }catch(e){
+        return error
+    }
+}
+
+const updateMovieById = async (id, body) => {
+    const {name, description, year, favorite} = body
+
+    try {
+        const updateMovie = await db.one("UPDATE movies SET name=$1, description=$2, year=$3, favorite=$4 WHERE id=$5 RETURNING *", [name, description, year, favorite, id]);
+        return updateMovie
+    } catch (error) {
+        return error
+    }
+
+}
+
+const deleteMovie = async (id) => {
+    try{
+        const movie = await db.one("DELETE FROM movies WHERE id=$1 RETURNING *", id)
+        return movie
+    }catch (error) {
+        return error
+    }
+
+}
 module.exports = {
-    getAllMovies
+    getAllMovies,
+    getMovieById,
+    createMovie,
+    updateMovieById,
+    deleteMovie
+    
 }
